ef7fb1b6ebc6d3bedc623d706dc5acfb151ea7ed
PRAVEEN IRULAPPAN <168902014+praveen7500@users.noreply.github.com>
2026-06-29 20:29:54 +0530

    Implement Snowflake connection and data loading
    Add function to connect and load data into Snowflake

--- scripts/load_to_snowflake.py
+++ scripts/load_to_snowflake.py
@@ -1,1 +1,28 @@
+import os
+import snowflake.connector
+from dotenv import load_dotenv
+from logger import logger
 
+load_dotenv()
+
+def load():
+
+    conn = snowflake.connector.connect(
+        account=os.getenv("SNOWFLAKE_ACCOUNT"),
+        user=os.getenv("SNOWFLAKE_USER"),
+        password=os.getenv("SNOWFLAKE_PASSWORD"),
+        warehouse=os.getenv("SNOWFLAKE_WAREHOUSE"),
+        database=os.getenv("SNOWFLAKE_DATABASE"),
+        schema=os.getenv("SNOWFLAKE_SCHEMA")
+    )
+
+    cursor = conn.cursor()
+
+    logger.info("Connected to Snowflake")
+
+    cursor.execute("SELECT CURRENT_VERSION()")
+
+    print(cursor.fetchone())
+
+    cursor.close()
+    conn.close()
