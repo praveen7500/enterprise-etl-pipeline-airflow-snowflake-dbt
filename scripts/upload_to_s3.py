90fd3e57038f57d313aff070417c9031d8364917
PRAVEEN IRULAPPAN <168902014+praveen7500@users.noreply.github.com>
2026-06-29 19:35:10 +0530

    Add S3 upload functionality with logging

--- scripts/upload_to_s3.py
+++ scripts/upload_to_s3.py
@@ -1,1 +1,16 @@
+import boto3
+from logger import logger
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+def upload(file_name):
+
+    bucket = os.getenv("S3_BUCKET")
+
+    s3 = boto3.client("s3")
+
+    s3.upload_file(file_name, bucket, file_name)
+
+    logger.info("Uploaded to S3")
