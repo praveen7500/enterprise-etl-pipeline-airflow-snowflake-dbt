2466b013180c9b28f095382826b833f9aa1a5e2d
PRAVEEN IRULAPPAN <168902014+praveen7500@users.noreply.github.com>
2026-06-29 21:11:29 +0530

    Update monitoring_dag.py

--- dags/monitoring_dag.py
+++ dags/monitoring_dag.py
@@ -1,1 +1,25 @@
+from airflow import DAG
 
+from airflow.operators.empty import EmptyOperator
+
+from datetime import datetime
+
+with DAG(
+
+    dag_id="pipeline_monitor",
+
+    schedule="@hourly",
+
+    start_date=datetime(2026,1,1),
+
+    catchup=False
+
+) as dag:
+
+    start = EmptyOperator(task_id="start")
+
+    health = EmptyOperator(task_id="health_check")
+
+    finish = EmptyOperator(task_id="finish")
+
+    start >> health >> finish
