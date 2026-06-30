fbd638f7367c8b8f36b15cccb044937a6b76da79
PRAVEEN IRULAPPAN <168902014+praveen7500@users.noreply.github.com>
2026-06-30 21:46:55 +0530

    Update slack.py

--- notifications/slack.py
+++ notifications/slack.py
@@ -1,1 +1,18 @@
+import os
+import requests
+from dotenv import load_dotenv
 
+load_dotenv()
+
+WEBHOOK = os.getenv("SLACK_WEBHOOK")
+
+
+def send_slack(message):
+
+    payload = {
+
+        "text": message
+
+    }
+
+    requests.post(WEBHOOK, json=payload)
