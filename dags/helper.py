15424ede36cdc0b96e2c06669e62b56d36b6975b
PRAVEEN IRULAPPAN <168902014+praveen7500@users.noreply.github.com>
2026-06-30 22:10:28 +0530

    Update helper.py

--- dags/helper.py
+++ dags/helper.py
@@ -1,10 +1,21 @@
+import sys
 import subprocess
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(message)s"
+)
+
+logger = logging.getLogger(__name__)
+
 
 def execute_python(script):
+    logger.info(f"Running {script}")
 
     subprocess.run(
-
-        ["python", script],
-
+        [sys.executable, script],
         check=True
     )
+
+    logger.info(f"{script} completed successfully.")
