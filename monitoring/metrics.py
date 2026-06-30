b9f97f7f18d09a5176f20fd23143f82c1dc9e524
PRAVEEN IRULAPPAN <168902014+praveen7500@users.noreply.github.com>
2026-06-30 21:48:53 +0530

    Update metrics.py

--- monitoring/metrics.py
+++ monitoring/metrics.py
@@ -1,1 +1,14 @@
+import pandas as pd
 
+
+def metrics(df):
+
+    print("Rows :", len(df))
+
+    print("Columns :", len(df.columns))
+
+    print("Duplicates :", df.duplicated().sum())
+
+    print("Null Values")
+
+    print(df.isnull().sum())
