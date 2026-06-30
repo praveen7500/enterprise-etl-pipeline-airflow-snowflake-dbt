39cd356fe5e02cfeb0e4f3b021e4a0b04ed52541
PRAVEEN IRULAPPAN <168902014+praveen7500@users.noreply.github.com>
2026-06-30 21:47:26 +0530

    Implement email sending functionality using AWS SES

--- notifications/email.py
+++ notifications/email.py
@@ -1,1 +1,38 @@
+import boto3
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+ses = boto3.client("ses")
+
+FROM = os.getenv("EMAIL_FROM")
+
+TO = os.getenv("EMAIL_TO")
+
+
+def send_email(subject, body):
+
+    ses.send_email(
+
+        Source=FROM,
+
+        Destination={"ToAddresses": [TO]},
+
+        Message={
+
+            "Subject": {"Data": subject},
+
+            "Body": {
+
+                "Text": {
+
+                    "Data": body
+
+                }
+
+            }
+
+        }
+
+    )
