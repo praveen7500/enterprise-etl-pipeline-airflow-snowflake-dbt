ac367f91aabf8a2c53d1a37a42ff4b554dab49f5
PRAVEEN IRULAPPAN <168902014+praveen7500@users.noreply.github.com>
2026-06-30 21:47:13 +0530

    Update sns.py

--- notifications/sns.py
+++ notifications/sns.py
@@ -1,1 +1,21 @@
+import boto3
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+sns = boto3.client("sns")
+
+TOPIC = os.getenv("SNS_TOPIC_ARN")
+
+
+def send_sns(subject, message):
+
+    sns.publish(
+
+        TopicArn=TOPIC,
+
+        Subject=subject,
+
+        Message=message
+    )
